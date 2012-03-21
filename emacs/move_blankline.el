b6c26ac8efcc1d7424a50700558a436c7cc5771e
yahuang <yao-long.huang@brion.com>
2012-03-21 09:52:58 +0800

    Add repo description and emacs list functions that move to the blank line.

---
+++ emacs/move_blankline.el
@@ -0,0 +1,16 @@
+(defun last-blank-block ()
+ (interactive)
+ (search-backward-regexp "^[ \t]*[^ \t\n]+.*\n\\([ \t]*\n\\)+"
+  (point-min)
+  1)
+ (next-line))
+
+(defun next-blank-block ()
+ (interactive)
+ (search-forward-regexp "\\(\n[ \t]*\\)+\n[ \t]*[^ \t\n]+"
+  (point-max)
+  1)
+ (move-beginning-of-line 0))
+
+(global-set-key (kbd "C-{") 'last-blank-block)
+(global-set-key (kbd "C-}") 'next-blank-block)')'}"))
