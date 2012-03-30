f9beb1332d72a614cab7318f4adf3474989d234e
Yaolong Huang <airekans@gmail.com>
2012-03-30 21:56:08 +0800

    etagswrap: create the dir if the dir not exist.

--- bash/etagswrap.sh
+++ bash/etagswrap.sh
@@ -3,20 +3,22 @@
 if [ $# -lt 2 ]
 then
     echo "Please give directory and extension to the command"
-    exit 0
-fi
-
-if [ ! -f "$tag_file_path" ]
-then
-    touch $tag_file_path
+    exit 1
 fi
 
 root_dir="$1"
 file_ext="$2"
 tag_file_path=~/.emacs.d/tags/TAGS.auto_generated
 
-echo $root_dir
-echo $file_ext
+if [ ! -f "$tag_file_path" ]
+then
+    tag_file_dir=$(dirname $tag_file_path)
+    if [ ! -e "$tag_file_dir" ]
+    then
+        mkdir -p $tag_file_dir
+    fi
+    touch $tag_file_path
+fi
 
 find $root_dir -name "*.$file_ext" -print \
     -exec etags -a -o $tag_file_path {} \;
