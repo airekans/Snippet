86170f1c85f18423e3178a0a296f6e73dd046b17
airekans <airekans@gmail.com>
2012-03-30 08:40:46 +0800

    Add etagswrap.sh to ease creating tags for emacs.

---
+++ bash/etagswrap.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+if [ $# -lt 2 ]
+then
+    echo "Please give directory and extension to the command"
+fi
+
+root_dir="$1"
+file_ext="$2"
+tag_file_path=~/.emacs.d/tags/TAGS.auto_generated
+
+echo $root_dir
+echo $file_ext
+
+find $root_dir -name "*.$file_ext" -print \
+    -exec etags -a -o $tag_file_path {} \;
+
