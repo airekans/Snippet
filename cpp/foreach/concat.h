f1dcb34bfc420809b68884260247a14e75776372
Yaolong Huang <airekans@gmail.com>
2013-03-08 12:31:30 +0800

    Fix a __i386 bug in gcc
    In gcc, __i386 has special meaning for i386 CPU.
    So we should avoid use __i386 as variable name.

--- cpp/foreach/concat.h
+++ cpp/foreach/concat.h
@@ -2,7 +2,7 @@
 #define _CONCAT_H_
 
 /// Utitilies to make a unique variable name.
-#define CONCAT_LINE(x) CONCAT(x, __LINE__)
+#define CONCAT_LINE(x) CONCAT(x ## _foreach__, __LINE__)
 #define CONCAT(a, b) CONCAT_INDIRECT(a, b)
 #define CONCAT_INDIRECT(a, b) a ## b
 
