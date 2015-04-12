825a3c95f86db99d955dacdfba6b263435a3a22e
Yaolong Huang <airekans@gmail.com>
2015-04-12 23:26:09 +0800

    Add HashMap to algo #3
    Add BM for const find and sequential find.

--- cpp/algo/benchmark/HashMapBenchmark.cpp
+++ cpp/algo/benchmark/HashMapBenchmark.cpp
@@ -85,6 +85,109 @@ BENCHMARK_TEMPLATE(BM_StdMapInsert, std::string)->Range(8, 8<<10);
 BENCHMARK_TEMPLATE(BM_StdUnorderedMapInsert, std::string)->Range(8, 8<<10);
 
 
+template<typename C, typename T>
+static void BM_HashMapConstFind(benchmark::State& state)
+{
+    C hash_map;
+    T value = GetValue<T>::Get();
+    for (int i = 0; i < state.range_x(); ++i)
+    {
+        hash_map[i] = value;
+    }
+    int key_to_find = state.range_x() - 1;
+
+    while (state.KeepRunning())
+    {
+        for (int i = 0; i < state.range_x(); ++i)
+        {
+            hash_map.Find(key_to_find, value);
+        }
+    }
+}
+
+template<typename C, typename T>
+static void BM_StdMapConstFind(benchmark::State& state)
+{
+    C hash_map;
+    T value = GetValue<T>::Get();
+    for (int i = 0; i < state.range_x(); ++i)
+    {
+        hash_map[i] = value;
+    }
+    int key_to_find = state.range_x() - 1;
+
+    while (state.KeepRunning())
+    {
+        for (int i = 0; i < state.range_x(); ++i)
+        {
+            hash_map.find(key_to_find);
+        }
+    }
+}
+
+typedef HashMap<int, int> IntHashMap;
+typedef std::map<int, int> IntStdMap;
+typedef std::tr1::unordered_map<int, int> IntUnorderedMap;
+
+typedef HashMap<int, std::string> StrHashMap;
+typedef std::map<int, std::string> StrStdMap;
+typedef std::tr1::unordered_map<int, std::string> StrUnorderedMap;
+
+BENCHMARK_TEMPLATE2(BM_HashMapConstFind, IntHashMap, int)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE2(BM_StdMapConstFind, IntStdMap, int)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE2(BM_StdMapConstFind, IntUnorderedMap, int)->Range(8, 8<<10);
+
+BENCHMARK_TEMPLATE2(BM_HashMapConstFind, StrHashMap, std::string)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE2(BM_StdMapConstFind, StrStdMap, std::string)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE2(BM_StdMapConstFind, StrUnorderedMap, std::string)->Range(8, 8<<10);
+
+template<typename C, typename T>
+static void BM_HashMapSeqFind(benchmark::State& state)
+{
+    C hash_map;
+    T value = GetValue<T>::Get();
+    for (int i = 0; i < state.range_x(); ++i)
+    {
+        hash_map[i] = value;
+    }
+
+    while (state.KeepRunning())
+    {
+        for (int i = 0; i < state.range_x(); ++i)
+        {
+            hash_map.Find(i, value);
+        }
+    }
+}
+
+template<typename C, typename T>
+static void BM_StdMapSeqFind(benchmark::State& state)
+{
+    C hash_map;
+    T value = GetValue<T>::Get();
+    for (int i = 0; i < state.range_x(); ++i)
+    {
+        hash_map[i] = value;
+    }
+
+    while (state.KeepRunning())
+    {
+        for (int i = 0; i < state.range_x(); ++i)
+        {
+            hash_map.find(i);
+        }
+    }
+}
+
+BENCHMARK_TEMPLATE2(BM_HashMapSeqFind, IntHashMap, int)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE2(BM_StdMapSeqFind, IntStdMap, int)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE2(BM_StdMapSeqFind, IntUnorderedMap, int)->Range(8, 8<<10);
+
+BENCHMARK_TEMPLATE2(BM_HashMapSeqFind, StrHashMap, std::string)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE2(BM_StdMapSeqFind, StrStdMap, std::string)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE2(BM_StdMapSeqFind, StrUnorderedMap, std::string)->Range(8, 8<<10);
+
+
 BENCHMARK_MAIN();
 
 
