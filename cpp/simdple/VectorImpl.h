da4b8bdb7147bfa694cb07ed51c1707519ca25af
Yaolong Huang <airekans@gmail.com>
2016-11-01 23:35:57 +0800

    Add And, AndNot and IsEqual.

--- cpp/simdple/VectorImpl.h
+++ cpp/simdple/VectorImpl.h
@@ -9,28 +9,38 @@ namespace detail {
 template<bool> struct StaticAssert;
 template<> struct StaticAssert<true> { typedef int Type; };
 
-template<typename T, unsigned int N> struct GeneralPack;
-template<> struct GeneralPack<char, 16> { typedef __m128i Type; };
-template<> struct GeneralPack<short, 16> { typedef __m128i Type; };
-template<> struct GeneralPack<int, 16> { typedef __m128i Type; };
-template<> struct GeneralPack<long long, 16> { typedef __m128i Type; };
-template<> struct GeneralPack<float, 16> { typedef __m128 Type; };
-template<> struct GeneralPack<double, 16> { typedef __m128d Type; };
+template<unsigned int N>
+struct AssertIsPowerOf2
+{
+    typedef typename StaticAssert<(N > 0) && ((N & (N - 1)) == 0)>::Type Type;
+};
 
-template<typename T, typename VT, unsigned int N>
+
+template<typename T, typename VT, typename GVT, unsigned int N>
 struct Pack
 {
     typedef T ElemType;
     typedef VT VType;
-    typedef typename GeneralPack<T, sizeof(VT)>::Type GeneralVT;
+    typedef GVT GeneralVT;
     enum { ElemNum = N };
     typedef typename StaticAssert<(sizeof(VT) / sizeof(T)) == N>::Type _ASSERT;
+    typedef typename AssertIsPowerOf2<N>::Type _IS_POWER_OF_2;
 
     union {
         VT v;
         GeneralVT gv;
         T data[N];
     };
+
+    inline T& operator[](size_t i)
+    {
+        return data[i];
+    }
+
+    inline T operator[](size_t i) const
+    {
+        return data[i];
+    }
 };
 
 }  // namespace detail
@@ -38,26 +48,26 @@ struct Pack
 template<typename T, unsigned int N>
 struct VectorImpl;
 
-template<> struct VectorImpl<char, 16> : public detail::Pack<char, __v16qi, 16>
+template<> struct VectorImpl<char, 16> : public detail::Pack<char, __v16qi, __m128i, 16>
 {
-    static VectorImpl Load(char elem)
+    inline static VectorImpl Load(char elem)
     {
         VectorImpl res;
         res.Set(elem);
         return res;
     }
 
-    void Set(char elem)
+    inline void Set(char elem)
     {
         gv = _mm_set1_epi8(elem);
     }
 
-    void AddFrom(const VectorImpl other)
+    inline void AddFrom(const VectorImpl other)
     {
         gv = _mm_add_epi8(gv, other.gv);
     }
 
-    VectorImpl Add(const VectorImpl other) const
+    inline VectorImpl Add(const VectorImpl other) const
     {
         VectorImpl res;
         res.gv = gv;
@@ -65,40 +75,75 @@ template<> struct VectorImpl<char, 16> : public detail::Pack<char, __v16qi, 16>
         return res;
     }
 
-    void SubFrom(const VectorImpl other)
+    inline void SubFrom(const VectorImpl other)
     {
         gv = _mm_sub_epi8(gv, other.gv);
     }
 
-    VectorImpl Sub(const VectorImpl other) const
+    inline VectorImpl Sub(const VectorImpl other) const
     {
         VectorImpl res;
         res.gv = gv;
         res.SubFrom(other);
         return res;
     }
+
+    inline void AndFrom(const VectorImpl other)
+    {
+        gv = _mm_and_si128(gv, other.gv);
+    }
+
+    inline VectorImpl And(const VectorImpl other) const
+    {
+        VectorImpl res;
+        res.gv = gv;
+        res.AndFrom(other);
+        return res;
+    }
+
+    // (!a) & b
+    // This can be used in expression template to optimize
+    inline void AndNotFrom(const VectorImpl other)
+    {
+        gv = _mm_andnot_si128(gv, other.gv);
+    }
+
+    inline VectorImpl AndNot(const VectorImpl other) const
+    {
+        VectorImpl res;
+        res.gv = gv;
+        res.AndNotFrom(other);
+        return res;
+    }
+
+    inline VectorImpl IsEqual(const VectorImpl other) const
+    {
+        VectorImpl res;
+        res.gv = _mm_cmpeq_epi8(gv, other.gv);
+        return res;
+    }
 };
 
-template<> struct VectorImpl<short, 8> : public detail::Pack<short, __v8hi, 8>
+template<> struct VectorImpl<short, 8> : public detail::Pack<short, __v8hi, __m128i, 8>
 {
-    static VectorImpl Load(short elem)
+    inline static VectorImpl Load(short elem)
     {
         VectorImpl res;
         res.Set(elem);
         return res;
     }
 
-    void Set(short elem)
+    inline void Set(short elem)
     {
         gv = _mm_set1_epi16(elem);
     }
 
-    void AddFrom(const VectorImpl other)
+    inline void AddFrom(const VectorImpl other)
     {
         gv = _mm_add_epi16(gv, other.gv);
     }
 
-    VectorImpl Add(const VectorImpl other) const
+    inline VectorImpl Add(const VectorImpl other) const
     {
         VectorImpl res;
         res.gv = gv;
@@ -106,12 +151,12 @@ template<> struct VectorImpl<short, 8> : public detail::Pack<short, __v8hi, 8>
         return res;
     }
 
-    void SubFrom(const VectorImpl other)
+    inline void SubFrom(const VectorImpl other)
     {
         gv = _mm_sub_epi16(gv, other.gv);
     }
 
-    VectorImpl Sub(const VectorImpl other) const
+    inline VectorImpl Sub(const VectorImpl other) const
     {
         VectorImpl res;
         res.gv = gv;
@@ -120,21 +165,27 @@ template<> struct VectorImpl<short, 8> : public detail::Pack<short, __v8hi, 8>
     }
 };
 
-template<> struct VectorImpl<int, 4> : public detail::Pack<int, __v4si, 4> {};
-template<> struct VectorImpl<long long, 2> : public detail::Pack<long long, __v2di, 2> {};
-template<> struct VectorImpl<float, 4> : public detail::Pack<float, __v4sf, 4> {};
-template<> struct VectorImpl<double, 2> : public detail::Pack<double, __v2df, 2> {};
+template<> struct VectorImpl<int, 4> : public detail::Pack<int, __v4si, __m128i, 4> {};
+template<> struct VectorImpl<long long, 2> : public detail::Pack<long long, __v2di, __m128i, 2> {};
+template<> struct VectorImpl<float, 4> : public detail::Pack<float, __v4sf, __m128, 4> {};
+template<> struct VectorImpl<double, 2> : public detail::Pack<double, __v2df, __m128d, 2> {};
 
 template<typename T, unsigned int N>
-VectorImpl<T, N> operator+(const VectorImpl<T, N> lhs, const VectorImpl<T, N> rhs)
+inline VectorImpl<T, N> operator+(const VectorImpl<T, N> lhs, const VectorImpl<T, N> rhs)
 {
     return lhs.Add(rhs);
 }
 
 template<typename T, unsigned int N>
-VectorImpl<T, N> operator-(const VectorImpl<T, N> lhs, const VectorImpl<T, N> rhs)
+inline VectorImpl<T, N> operator-(const VectorImpl<T, N> lhs, const VectorImpl<T, N> rhs)
 {
     return lhs.Sub(rhs);
+}
+
+template<typename T, unsigned int N>
+inline VectorImpl<T, N> operator&(const VectorImpl<T, N> lhs, const VectorImpl<T, N> rhs)
+{
+    return lhs.And(rhs);
 }
 
 }  // namespace simdple
--- cpp/simdple/tests/VectorImplTest.cpp
+++ cpp/simdple/tests/VectorImplTest.cpp
@@ -17,16 +17,16 @@ TYPED_TEST(VectorImplTest, TestAdd)
     TypeParam v_3 = v.Add(v_2);
     TypeParam v_4 = v_3.Add(v);
     TypeParam v_5 = v_4.Add(v_3);
-    ASSERT_EQ(1, v_5.data[0]);
+    ASSERT_EQ(1, v_5[0]);
 
     v.AddFrom(v_2);
     v_3.AddFrom(v);
-    ASSERT_EQ(1, v_3.data[0]);
+    ASSERT_EQ(1, v_3[0]);
 
     v = v_2 + v_5;
     v_4 = v + v_3;
     v_3 = v_5 + v_2 + v + v_4;
-    ASSERT_EQ(1, v_3.data[0]);
+    ASSERT_EQ(1, v_3[0]);
 }
 
 TYPED_TEST(VectorImplTest, TestSub)
@@ -36,6 +36,6 @@ TYPED_TEST(VectorImplTest, TestSub)
     TypeParam v_3 = v.Sub(v_2);
     TypeParam v_4 = v_3.Sub(v);
     TypeParam v_5 = v_4.Sub(v_3);
-    ASSERT_EQ(1, v_5.data[0]);
+    ASSERT_EQ(1, v_5[0]);
 }
 
