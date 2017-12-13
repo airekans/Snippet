bcd8b9272c55fcfba625ad35477919f6cbfc9dd0
Yaolong Huang <airekans@gmail.com>
2017-12-12 17:20:04 -0800

    Fix format issue

--- cpp/simdple/VectorImplAVX.h
+++ cpp/simdple/VectorImplAVX.h
@@ -11,26 +11,12 @@ template<> struct VectorImpl<char, 32> :
     public detail::IntegerPack<char, __v32qi, __m256i, 32>
 {
     // addr must be 256-bit aligned
-    inline void Set(const char* addr)
-    {
-        gv = _mm256_load_si256(reinterpret_cast<const GeneralVT*>(addr));
-    }
-
-    inline void Set(char elem)
-    {
-        gv = _mm256_set1_epi8(elem);
-    }
+    inline void Set(const char* addr) { gv = _mm256_load_si256(reinterpret_cast<const GeneralVT*>(addr)); }
+    inline void Set(char elem) { gv = _mm256_set1_epi8(elem); }
 
 #ifdef __AVX2__
-    inline void AddFrom(const VectorImpl other)
-    {
-        gv = _mm256_add_epi8(gv, other.gv);
-    }
-
-    inline void SubFrom(const VectorImpl other)
-    {
-        gv = _mm256_sub_epi8(gv, other.gv);
-    }
+    inline void AddFrom(const VectorImpl other) { gv = _mm256_add_epi8(gv, other.gv); }
+    inline void SubFrom(const VectorImpl other) { gv = _mm256_sub_epi8(gv, other.gv); }
 
 #endif // __AVX2__
 };
