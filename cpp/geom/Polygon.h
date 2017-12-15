b968944764019f1dd1a170f72475e94f4574ffd1
Yaolong Huang <airekans@gmail.com>
2017-12-14 18:50:03 -0800

    Finished part of Polygon

--- cpp/geom/Polygon.h
+++ cpp/geom/Polygon.h
@@ -1,10 +1,24 @@
 #ifndef GEOM_POLYGON_H_
 #define GEOM_POLYGON_H_
 
+#include <vector>
+
+#include "Point.h"
+
 namespace geom {
 
+template<typename T>
 class Polygon
 {
+public:
+     typedef Point2D<T> PointType;
+
+     std::size_t GetPointSize() const { return m_points.size(); }
+     const PointType& GetPoint(std::size_t i) const { return m_points[i]; }
+     void SetPoint(std::size_t i, const PointType& p) { m_points[i] = p; }
+
+private:
+     std::vector<PointType> m_points;
 };
 
 }  // namespace geom
